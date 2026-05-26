5115ff0bd842e1f9bfd5a23f9d749e6c4baf3033
VladaS20 <cssnooo3641@mail.ru>
2026-05-26 14:22:11 +0300

    Update single.js

--- js/single.js
+++ js/single.js
@@ -1,23 +1,33 @@
-(function () {
-  "use strict";
-  document.addEventListener("DOMContentLoaded", function () {
-    var buyBtn = document.getElementById("buy-btn");
-    if (!buyBtn) return;
+// (function () {
+//   "use strict";
+//   document.addEventListener("DOMContentLoaded", function () {
+//     var buyBtn = document.getElementById("buy-btn");
+//     if (!buyBtn) return;
 
-    function updateSingleBuyBtn() {
-      if (window.innerWidth <= 992) {
-        if (buyBtn.getAttribute("href")) {
-          buyBtn.dataset.href = buyBtn.getAttribute("href");
-          buyBtn.removeAttribute("href");
-        }
-      } else {
-        if (!buyBtn.getAttribute("href") && buyBtn.dataset.href) {
-          buyBtn.setAttribute("href", buyBtn.dataset.href);
-        }
-      }
-    }
+//     function updateSingleBuyBtn() {
+//       if (window.innerWidth <= 992) {
+//         if (buyBtn.getAttribute("href")) {
+//           buyBtn.dataset.href = buyBtn.getAttribute("href");
+//           buyBtn.removeAttribute("href");
+//         }
+//       } else {
+//         if (!buyBtn.getAttribute("href") && buyBtn.dataset.href) {
+//           buyBtn.setAttribute("href", buyBtn.dataset.href);
+//         }
+//       }
+//     }
 
-    updateSingleBuyBtn();
-    window.addEventListener("resize", updateSingleBuyBtn);
+//     updateSingleBuyBtn();
+//     window.addEventListener("resize", updateSingleBuyBtn);
+//   });
+// })();
+
+document.getElementById('buy-btn').addEventListener('click', function(e) {
+  e.preventDefault();
+  radario.Widgets.Event({
+    params: { textBtnColor: '#FFFFFF' },
+    standalone: false,
+    createButton: false,
+    eventId: 2718589
   });
-})();
+});
